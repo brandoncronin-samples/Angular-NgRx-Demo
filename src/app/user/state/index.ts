f22abb332fd2a240f23f9271066836462661e845
Brandon Cronin <brandon.cronin@gmail.com>
2018-11-03 15:34:56 -0700

    Initial Commit

---
+++ src/app/user/state/index.ts
@@ -0,0 +1,15 @@
+import { createSelector, createFeatureSelector } from "@ngrx/store";
+import { UserState } from "./user.reducer";
+
+// Selector functions
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+    getUserFeatureState,
+    state => state.maskUserName
+);
+
+export const getCurrentUser = createSelector(
+    getUserFeatureState,
+    state => state.currentUser
+);
